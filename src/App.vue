6a16bb47f8816025391c8177f4a9ba400280e238
Diogo Soares <diogo@jera.com.br>
2023-08-03 22:04:09 -0400

    Pokemons

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Pokemon from './components/Pokemon.vue';
+import { ref } from 'vue';
+
+const pokemons = ref([]);
+
+fetch("https://pokeapi.co/api/v2/pokemon").then( (res) => {
+  return res.json();
+}).then( (json) => {
+  pokemons.value = json.results;
+}).catch( (err) => {
+  console.log("deu ruim...", err);
+})
+</script>
+
+<template>
+  
+  <h1>Pokemons</h1>
+  <div v-for="pokemon in pokemons">
+    <Pokemon :pokemon="pokemon" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,33 @@
+<script setup>
+	defineProps({
+		pokemon: {
+			type: Object,
+			required: true
+		}
+	})
+</script>
+
+<template>
+  <div>
+    <div>
+      <span class="titulo">Nome</span>
+      <span class="conteudo">{{ pokemon.name }}</span>
+    </div>
+    <div>
+      <span class="titulo">Xp base</span>
+      <span class="conteudo">{{ pokemon.xpBase }}</span>
+    </div>
+    <div>
+      <span class="titulo">Peso</span>
+      <span class="conteudo">{{ pokemon.peso }}</span>
+    </div>
+    <div>
+      <h3>Movimentos</h3>
+      <div>
+        <p v-for="movimento in pokemon.movimentos" :key=movimento>
+					{{ movimento }}
+				</p>
+      </div>
+    </div>
+  </div>
+</template>
